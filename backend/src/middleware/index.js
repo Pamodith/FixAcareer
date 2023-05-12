eace52f2f4eb1d85222d07b8bc149ff02f706ae0
Pamuditha <99198913+Pamodith@users.noreply.github.com>
2023-05-12 20:11:18 +0530

    Initial commit

---
+++ backend/src/middleware/index.js
@@ -0,0 +1,3 @@
+export * from './errors';
+export * from './interceptors';
+export * from './rateLimiter';
---
+++ backend/src/utils/response.js
@@ -0,0 +1,15 @@
+import { responseInterceptor } from '../middleware';
+
+export const toSuccess = ({ res, status = 200, data, message }) => {
+  responseInterceptor({}, res, () => {});
+  if (res.polyglot) message = res.polyglot.t(message);
+  const responseData = { data, message };
+  if (!data) delete responseData.data;
+  res.status(status).json(responseData);
+};
+
+export const toError = ({ res, status = 500, message }) => {
+  responseInterceptor({}, res, () => {});
+  if (res.polyglot) message = res.polyglot.t(message);
+  res.status(status).json({ message });
+};
