ed71c2737540b05f9232b2adedc26904c937ffc7
Pamodith <P.M.TikithanthriMahasamilage@wlv.ac.uk>
2023-05-27 12:59:55 +0530

    Small Fix

--- frontend/src/layout/DashboardHeader/DashboardHeader.tsx
+++ frontend/src/layout/DashboardHeader/DashboardHeader.tsx
@@ -79,7 +79,7 @@ const DashboardHeader: React.FC = () => {
         <Group position="apart">
           <Image
             src={logo}
-            alt="Tax Management System"
+            alt="FixAcareer"
             width={100}
             height={40}
             fit="contain"
